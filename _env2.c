6bc88b3287d7a3b2ef200f517790643074cf8393
missnike <adesinaadedamola4@gmail.com>
2023-11-12 22:00:36 +0000

    splliting environment

---
+++ _env2.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/*
+*_envpath - A program that get envnv variable
+* @line: an argument
+* Return: the path
+ */
+
+char *_envpath(const char *line)
+{	
+	int lenght = lenstr(line);
+	char **envn = NULL;
+
+
+	envn =  environ;
+	while (*envn != NULL)
+	{
+	if (strncmp_made(line, *envn, lenght) == 0 && (*envn)[lenght] == '=')
+	{
+	return (&(*envn)[lenght + 1]);
+	}
+	envn++;
+	}
+	return (NULL);
+}
+
+
+/**
+ * _exec - a function the execute command
+ * @strargs: takes in command
+ * Return: void
+ */
+
+void _exec(char **strargs)
+{
+	char *command = NULL;
+	char *_strarg = NULL;
+
+	_strarg = strargs[0];
+	command = _location(_strarg);
+
+	if (execve(command, strargs, NULL) == -1)
+	{
+		perror("command not found");
+		exit(1);
+	}
+}
---
+++ _splitting.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * split_str- program the split string into two
+ * @str: string to be tokenalized
+ * @delim: slipting params
+ * Return: returns the slipted string
+ */
+char **split_str(char *str, const char *delim)
+{
+    int counttok = 0, i;
+    char *tokcpy = _strdup(str);
+    char **args = NULL;
+    char *argtok;
+
+    argtok = strtok(str, delim);
+    while (argtok != NULL)
+    {
+     counttok++;
+        argtok = strtok(NULL, delim);
+    }
+
+    args = (char **)malloc(sizeof(char *) *  (counttok + 1));
+    if (args == NULL)
+    {
+        perror("allocated memory failed");
+        exit(98);
+    }
+
+    argtok = strtok(tokcpy, delim);
+    for (i = 0; argtok != NULL; i++)
+    {
+        args[i] = _strdup(argtok);
+        argtok = strtok(NULL, delim);
+    }
+
+    args [counttok] = NULL;
+
+
+    free(tokcpy);
+
+    return args;
+}
